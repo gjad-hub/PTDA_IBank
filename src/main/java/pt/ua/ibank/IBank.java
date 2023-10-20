ee92ec5f58c266ac49889fe74c3b54406575c568
Gonçalo <goncalojosedias@live.com.pt>
2023-10-20 16:29:21 +0100

    First commit

---
+++ src/main/java/pt/ua/ibank/IBank.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pt.ua.ibank;
+
+/**
+ *
+ * @author gjad
+ */
+public class IBank {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
