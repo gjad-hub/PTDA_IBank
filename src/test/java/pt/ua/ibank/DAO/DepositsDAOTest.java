d8657e8dd1cd74ec6af8c28305742ce35536e55f
Asus <richardmiranda@ua.pt>
2024-01-07 15:08:07 +0000

    cartaodaotest deositsdaotest paymentsdaotest alguns dão erro ainda não sei porquê

---
+++ src/test/java/pt/ua/ibank/DAO/DepositsDAOTest.java
@@ -0,0 +1,36 @@
+package pt.ua.ibank.DAO;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepositsDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void requestDeposit() {
+    }
+
+    @Test
+    public void testRequestDepositSuccess() {
+        double testValor = 100.0;
+        int testCliente = 3;
+        assertEquals(DepositsDAO.codigoSucesso, DepositsDAO.requestDeposit(testValor, testCliente));
+    }
+
+    @Test
+    public void testRequestDepositErrorNonexistentClient() {
+        double testValor = 10.0;
+        int testCliente = 9999;
+        assertEquals(DepositsDAO.codigoErro, DepositsDAO.requestDeposit(testValor, testCliente));
+    }
+}
